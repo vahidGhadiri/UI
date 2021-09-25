0f71901e4d4c0bbfebcd34503824f9c1b15543ea
vahidGhadiri <whydrf@gmail.com>
2021-09-25 23:20:51 +0330

    [Feature]- Adding axios methods

--- login-logout/src/helper/http.js
+++ login-logout/src/helper/http.js
@@ -7,15 +7,40 @@ class HttpServices {
         axios.defaults.baseURL = BASE_URL
         axios.defaults.timeout = 10000
 
+        // Do something before request is sent
         axios.interceptors.request.use((config) => {
             return config
+        }, (error) => {
+            return Promise.reject(error)
         })
 
-
-        axios.interceptors.response.use((config) => {
-            return config
+        // Do something with response data
+        axios.interceptors.response.use((response) => {
+            return response
+        }, (error) => {
+            return Promise.reject(error)
         })
     }
+
+    get(url, config) {
+        return axios.get(url, config)
+    }
+
+    post(url, data, config) {
+        config = config || {headers: {"content-type": "application/json"}}
+        return axios.post(url, data, config)
+    }
+
+    put(url, data, config) {
+        config = config || {headers: {"content-type": "application/json"}}
+        return axios.put(url, data, config)
+    }
+
+    delete(url, data, config) {
+        config = config || {headers: {"content-type": "application/json"}}
+        return axios.delete(url, data, config)
+    }
 }
+
 
 export default new HttpServices()

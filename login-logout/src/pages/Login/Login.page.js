bff938d73bc45474872c0314c82d63dba4631216
vahidGhadiri <whydrf@gmail.com>
2021-09-27 23:05:57 +0330

    [Feature]- Adding animations & styles to Inputs

--- login-logout/src/pages/Login/Login.page.js
+++ login-logout/src/pages/Login/Login.page.js
@@ -1,23 +1,30 @@
 import React from "react"
 
 import {Input} from "../../components";
-import style from "./sample.module.scss"
+import style from "./Login.module.scss"
+import image from '../../asset/images/image.jpg'
 
 
 const Login = ({handleClick, handleChangeValue}) => {
     return (
         <div className={style.container}>
-            <Input
-                onChange={(e) => handleChangeValue(e.target.value)}
-                onClick={() => handleClick()}
-                label="Email"
-            />
-            <Input
-                onChange={(e) => handleChangeValue(e.target.value)}
-                onClick={() => handleClick()}
-                label="Password"
-                type="password"
-            />
+
+            <div className={style.formContainer}>
+                <Input
+                    onChange={(e) => handleChangeValue(e.target.value)}
+                    onClick={() => handleClick()}
+                    label="Email"
+                />
+                <Input
+                    onChange={(e) => handleChangeValue(e.target.value)}
+                    onClick={() => handleClick()}
+                    label="Password"
+                    type="password"
+                />
+            </div>
+            <div className={style.imageContainer}>
+                <img src={image} alt=""/>
+            </div>
         </div>
     )
 }
